439e04ea2ac702607977c4e895818f320c76bcc9
Ankit-69k <ankitkumarray19@gmail.com>
2025-09-21 21:15:02 +0530

    fix: update CORS configuration to allow specific origins and enable credentials

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ import {
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://aspire-map-frontend.vercel.app'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
